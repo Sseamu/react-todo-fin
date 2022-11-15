43c399c6f390f3e9978142c30ba23e90c5600be6
inspect8543@gmail.com <inspect8543@gmail.com>
2022-11-15 21:17:50 +0900

    second-react-netflix
    go to home need to upgrade

--- src/App.js
+++ src/App.js
@@ -1,104 +1,42 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
+import Form from "./components/form";
+import List from "./components/List";
 
-export default class App extends Component {
-  state = {
-    todoData: [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: false,
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false,
-      },
-    ],
-    value: "",
-  };
-  btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
+export default function App() {
+  console.log("App component");
+  const [todoData, setTodoData] = useState([]);
+
+  const [value, setValue] = useState("");
+
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      setTodoData(newTodoData);
+      localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    },
+    [todoData]
+  );
+  //할일 모두 지우기 버튼
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem("todoData", JSON.stringify([]));
   };
 
-  getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
-  handleClick = (id) => {
-    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
-    this.setState({ todoData: newTodoData });
-  };
-  handleChange = (e) => {
-    this.setState({ value: e.target.value });
-  };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let newTodo = {
-      id: Date.now(),
-      title: this.state.value,
-      completed: false,
-    };
-    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
-  };
-
-  handleCompleteChange = (id) => {
-    let newTodoData = this.state.todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    this.setState({ todoData: newTodoData });
-  };
-  render() {
-    return (
-      <div className="container">
-        <div className="todoBlock">
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-200">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할일 목록</h1>
-          {this.state.todoData.map((data) => (
-            <div style={this.getStyle(data.completed)} key={data.id}>
-              <input
-                type="checkbox"
-                defaultChecked={false}
-                onChange={() => this.handleCompleteChange(data.id)}
-              />
-              {data.title}
-              <button
-                style={this.btnStyle}
-                onClick={() => this.handleClick(data.id)}
-              >
-                x
-              </button>
-            </div>
-          ))}
-          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="value"
-              style={{ flex: "10", padding: "5px" }}
-              placeholder="해야할일을 입력하세요"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{ flex: "1" }}
-            />
-          </form>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
+        <List
+          todoData={todoData}
+          setTodoData={setTodoData}
+          handleClick={handleClick}
+        />
+        <Form setTodoData={setTodoData} value={value} setValue={setValue} />
       </div>
-    );
-  }
+    </div>
+  );
 }
